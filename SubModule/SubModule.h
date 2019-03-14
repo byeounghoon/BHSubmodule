ee4ec7fd33272865bb47abb4a1b786d66130bd84
ByoungHoon <futurum@bomapp.co.kr>
2019-03-14 13:48:51 +0900

    init

---
+++ SubModule/SubModule.h
@@ -0,0 +1,19 @@
+//
+//  SubModule.h
+//  SubModule
+//
+//  Created by ByoungHoon Yun on 14/03/2019.
+//  Copyright © 2019 Redvelvet Ventures Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SubModule.
+FOUNDATION_EXPORT double SubModuleVersionNumber;
+
+//! Project version string for SubModule.
+FOUNDATION_EXPORT const unsigned char SubModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubModule/PublicHeader.h>
+
+
