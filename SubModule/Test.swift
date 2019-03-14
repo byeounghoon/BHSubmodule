18e48e511ffe3a501ac0338ea7172413c399bc44
ByoungHoon <futurum@bomapp.co.kr>
2019-03-14 14:09:20 +0900

    update

---
+++ SubModule/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  SubModule
+//
+//  Created by ByoungHoon Yun on 14/03/2019.
+//  Copyright © 2019 Redvelvet Ventures Inc. All rights reserved.
+//
+
+import Foundation
+
+public struct Test {
+    let test : String
+}
